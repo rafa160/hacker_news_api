bb9babbc4ac7ec879df7fc7b2cb8f8f6877db3f0
Rafael Paz <rafaelpaz86@gmail.com>
2021-03-07 13:18:45 -0300

    Adiciona classe para tratar strings e text, colors e outros em widgets.

---
+++ lib/theme/constant.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+const appBackground = Color(0xFFEFF5F4);
+const backgroundColor =  Color.fromRGBO(58, 66, 86, 1.0);
+const primary = Color(0xFF00B7B7);
+const white = Color(0xFFFFFFFF);
+const textWhite = Color(0xFFFFFFFF);
+const textBlack = Color(0xFF000000);
+const textGrey = Colors.grey;
+const purple = Colors.purple;
+const yellowAccent = Colors.yellowAccent;
+
+
+const appTitle = TextStyle(
+    fontSize: 30,
+    fontWeight: FontWeight.bold,
+    height: 1.5,
+    color: textBlack
+);
+const appSubTitle = TextStyle(
+    fontSize: 25,
+    fontWeight: FontWeight.bold,
+    height: 1.5,
+    color: textBlack
+);
+const contentWhite = TextStyle(
+    fontSize: 15,
+    height: 1.5,
+    color: textWhite
+);
+const contentGrey = TextStyle(
+    fontSize: 15,
+    height: 1.5,
+    color: backgroundColor
+);
+const contentBlack = TextStyle(
+    fontSize: 18,
+    height: 1.5,
+    color: textBlack
+);
+const subContentGrey = TextStyle(
+    fontSize: 15,
+    height: 1.5,
+    color: textGrey
+);
+const titleAppName = TextStyle(
+    color: white,
+    fontSize: 30,
+    fontWeight: FontWeight.bold
+);
---
+++ lib/theme/strings.dart
@@ -0,0 +1,23 @@
+class Strings {
+
+  //login screen
+  static const String APP_NAME = 'News Hacker App';
+  static const String EMAIL_HINT = 'e-mail';
+  static const String PASSWORD_HINT = 'password';
+  static const String BUTTON_LOGIN = 'entrar';
+
+  //main screen
+  static const String TITLE_MAIN_PAGE = 'Hacker News';
+
+  //news details screen
+  static const String NO_COMMENTS = 'Não existe comentarios';
+
+  //Fav news screen
+  static const String FAV_TITLE = 'Favoritos';
+
+
+  //general
+  static const String NO_FILE = 'Nenhum Arquivo foi Encontrado';
+  static const String BTN_CLOSE_BUTTON = 'assets/images/btn_close.png';
+
+}
