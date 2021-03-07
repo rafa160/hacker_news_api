989b204c070090c004e0691b50a07ab8fca791ef
Rafael Paz <rafaelpaz86@gmail.com>
2021-03-07 13:13:27 -0300

    Provendo Blocs na Main.

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,21 @@
+import 'package:bloc_pattern/bloc_pattern.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_easyloading/flutter_easyloading.dart';
+import 'package:get/get_navigation/src/root/get_material_app.dart';
+import 'package:news_hacker_app/blocs/stories_bloc.dart';
+import 'package:news_hacker_app/blocs/user_bloc.dart';
 
 void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-      ),
-      home: Container(),
-    );
-  }
+  runApp(BlocProvider(
+    blocs: [
+      Bloc((i) => UserBloc()),
+      Bloc((i) => StoriesBloc())
+    ],
+    child: GetMaterialApp(
+      title: "News Hacker",
+      debugShowCheckedModeBanner: false,
+      builder: EasyLoading.init(),
+      home: SplashScreen(),
+    ),
+  ));
 }
